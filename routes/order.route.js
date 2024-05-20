1ca05856aa3dc77cb4dd2a15e61b7565eff7d041
VivekMahankali <153980921+VivekMahankali@users.noreply.github.com>
2024-05-21 01:23:38 +0530

    order.route

---
+++ routes/order.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const Order = require('../models/order');
+router.get('/user',enter)
+router.post('/order/:userId',getUserProducts)
+router.post('/order/cart',saveProduct)
+module.exports = router;
+
+
+ 
