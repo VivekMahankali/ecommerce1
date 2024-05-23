34672c7f861a99230e96b359191956bd81627fc6
sreeja <raosreeja5@gmail.com>
2024-05-23 05:47:54 +0530

    authenticate

---
+++ middleware/user.middleware.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_KEY, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+  res.status(200).json({ message: "Protected content", user: req.user });
+}
+
+module.exports = authenticateToken;
